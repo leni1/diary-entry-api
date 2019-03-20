b69bdd65f96fbf17b4c422feb631515e5e892d33
Leni Kadali <lenikmutungi@gmail.com>
2019-03-20 09:19:57 +0300

    ft: Add Helper class for handling validation and creation
    Added a Helper class for handling validation and creation
    of diary entries.

--- models/diary.py
+++ models/diary.py
@@ -1,6 +1,36 @@
+import re
+from datetime import date
+
+
 class Diary:
 
     def __init__(self, date, title, content):
         self.title = title
         self.date = date
         self.content = content
+
+
+class Helper:
+
+    def check_entry(self, title, content):
+
+        str_args = [title, content]
+
+        for arg in str_args:
+            if not isinstance(arg, str):
+                raise TypeError
+
+        valid_title = re.search(r'^[A-Za-z\s]', title)
+        valid_content = re.search(
+            r'^[\w\d!@#$%^&*()-+_/|}{":;\'><~`.,?\s]', content)
+
+        if valid_title and valid_content:
+            return True
+        return False
+
+    def create_entry(self, title, content):
+
+        if self.check_entry(title, content):
+            entry_date = date.today()
+            entry = Diary(entry_date, title, content)
+            return entry
