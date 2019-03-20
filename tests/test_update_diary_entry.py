33da0b6bd074695e351d814828cdc82192259d81
Leni Kadali <lenikmutungi@gmail.com>
2019-03-20 22:15:46 +0300

    [Finishes #164738291] Add minimal tests for update entry view endpoint
    Added minimal tests for update entry view point.

---
+++ tests/test_update_diary_entry.py
@@ -0,0 +1,59 @@
+import json
+
+from api import create_app
+
+
+def test_update_title_success():
+    test_app = create_app({'TESTING': True})
+    test_req = test_app.test_client()
+
+    new_req = test_req.post(
+        '/api/v1/entries', content_type='application/json',
+        data=json.dumps(
+            dict(
+                title='Failing test',
+                content='An interesting test.'
+            )
+        )
+    )
+
+    ent_id = json.loads(new_req.get_data())['Entry created']['eid']
+
+    resp = test_req.put(
+        '/api/v1/entries/{}'.format(ent_id),
+        content_type='application/json',
+        data=json.dumps(
+            dict(title='New Title')
+        )
+    )
+
+    assert resp.status_code == 201
+
+
+def test_update_content_success():
+    test_app = create_app({'TESTING': True})
+    test_req = test_app.test_client()
+
+    new_req = test_req.post(
+        '/api/v1/entries', content_type='application/json',
+        data=json.dumps(
+            dict(
+                title='Failing test',
+                content='An interesting test.'
+            )
+        )
+    )
+
+    ent_id = json.loads(new_req.get_data())['Entry created']['eid']
+
+    resp = test_req.put(
+        '/api/v1/entries/{}'.format(ent_id),
+        content_type='application/json',
+        data=json.dumps(
+            dict(
+                title='New title',
+                content='Some interesting content here.')
+        )
+    )
+
+    assert resp.status_code == 201
